3e97b9ad2009ed908b20c0c521c1334b74b7541b
Alex van Manen <alex@reisplanner.online>
2026-02-03 20:06:42 +0100

    Fix news sync: accept all statuses except rejected, add debug logging (v1.0.63)

--- wordpress-plugins/travelc-content/travelc-content.php
+++ wordpress-plugins/travelc-content/travelc-content.php
@@ -3,7 +3,7 @@
  * Plugin Name: TravelC Content
  * Plugin URI: https://travelcstudio.com
  * Description: Synchroniseert nieuws en bestemmingen van TravelCStudio naar WordPress. Content wordt beheerd in TravelCStudio en automatisch getoond op WordPress sites van brands die de content hebben geactiveerd.
- * Version: 1.0.62
+ * Version: 1.0.63
  * Author: RRP System
  * Author URI: https://rrpsystem.com
  * License: GPL v2 or later
@@ -15,7 +15,7 @@
 
 if (!defined('ABSPATH')) exit;
 
-define('TCC_VERSION', '1.0.62');
+define('TCC_VERSION', '1.0.63');
 define('TCC_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('TCC_PLUGIN_URL', plugin_dir_url(__FILE__));
 
@@ -1061,11 +1061,14 @@ class TravelC_Content {
         }
         
         // First get the news IDs that are activated for this brand
+        // Include all statuses except 'rejected' - if assigned, it should sync
         $assignments = $this->fetch_from_supabase('news_brand_assignments', array(
             'brand_id' => 'eq.' . $this->brand_id,
-            'status' => 'in.(accepted,mandatory)',
-            'select' => 'news_id'
+            'status' => 'neq.rejected',
+            'select' => 'news_id,status,is_published'
         ));
+        
+        error_log('[TCC] News assignments for brand ' . $this->brand_id . ': ' . print_r($assignments, true));
         
         if (is_wp_error($assignments) || empty($assignments)) {
             return array();
